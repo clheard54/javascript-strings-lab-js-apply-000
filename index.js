3ee78b9d46588c4e998c1659c8d670d0a6f78fb5
Clarion Heard <clahon54@gmail.com>
2019-11-06 19:22:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,4 +6,8 @@ var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
 var topic = "space";
 
+<<<<<<< HEAD
 var conversation = `Let's talk about ${topic}.`
+=======
+var conversation = `Let's talk about ${topic}.`;
+>>>>>>> e2db690b30eb3b21f08d30e8f9ea9313faa2b833
